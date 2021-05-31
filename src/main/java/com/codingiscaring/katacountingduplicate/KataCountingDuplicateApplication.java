a93da43ed5f8fd91a5fc19c4a622c718a2cb028f
yodralopez <yodralopez@gmail.com>
2021-05-31 09:22:51 +0100

    initial commit

---
+++ src/main/java/com/codingiscaring/katacountingduplicate/KataCountingDuplicateApplication.java
@@ -0,0 +1,13 @@
+package com.codingiscaring.katacountingduplicate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataCountingDuplicateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataCountingDuplicateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codingiscaring/katacountingduplicate/KataCountingDuplicateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingiscaring.katacountingduplicate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataCountingDuplicateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

701af6ceebb461be7af078439905a94a591d3b6c
Maria Soria <maria.it3@gmail.com>
2021-06-02 08:45:33 +0100

    Test to countDuplicates

--- src/test/java/com/codingiscaring/katacountingduplicate/CountingDuplicateShould.java
+++ src/test/java/com/codingiscaring/katacountingduplicate/CountingDuplicateShould.java
@@ -10,7 +10,44 @@ import static org.junit.jupiter.api.Assertions.*;
 class CountingDuplicateShould {
 
     @Test
-    void foo() {
-        assertEquals(4, 2 + 2);
+    void doesNotFindDuplicatedCharacters() {
+        assertEquals(0, CountingDuplicate.count("abcde"));
     }
+
+    @Test
+    void doesFindOneCharacterDuplicated() {
+        assertEquals(1, CountingDuplicate.count("indivisibility"));
+    }
+
+    @Test
+    void doesFindTwoCharacterDuplicated() {
+        assertEquals(2, CountingDuplicate.count("aabbcde"));
+    }
+
+    @Test
+    void doesRemoveDuplicates() {
+        assertEquals("abcde", CountingDuplicate.removeDuplicates("aabbcde"));
+    }
+
+    @Test
+    void doesFindLowerCaseAndUpperCaseCharactersDuplicated() {
+        assertEquals(2, CountingDuplicate.count("IndIvisibilities"));
+    }
+
+    @Test
+    void doesFindCharactersAndNumbersDuplicated() {
+        assertEquals(2, CountingDuplicate.count("aA11"));
+    }
+
+    @Test
+    void getFrequency() {
+        assertEquals(2, CountingDuplicate.getFrequency("aabcde", "a"));
+    }
+
+    @Test
+    void wordToList() {
+        List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+        assertEquals(expected, Arrays.asList("aabcd".split("")));
+    }
+
 }
